df5d9ca7b5a666b3b5bd0e6604e979d9b5d45056
cs-indo <cs-indo@users.noreply.github.com>
2026-07-30 07:52:58 +0000

    fix DrakorAsia: add missing Qualities import

--- DrakorAsia/src/main/kotlin/com/indo/DrakorAsia.kt
+++ DrakorAsia/src/main/kotlin/com/indo/DrakorAsia.kt
@@ -3,6 +3,7 @@ package com.indo
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
 import android.util.Base64
 
 class DrakorAsia : MainAPI() {
